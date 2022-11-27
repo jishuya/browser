cfedeae0b06925777f535905f1b4544caf57875a
jishuya <jishuya3015@gmail.com>
2022-11-27 16:16:58 +0900

    eventListner

---
+++ ch1/index.js
@@ -0,0 +1,4 @@
+// console.log('window.screen: ', window.screen)
+// console.log('window.outer: ', window.outer)
+// console.log('window.inner: ', window.inner)
+// console.log('documentElement.clientWidth: ', documentElement.clientWidth)
