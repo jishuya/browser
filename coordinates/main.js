07667cc8c76b07bd2538d8e6110f11993042c56b
jishuya <jishuya3015@gmail.com>
2022-12-26 23:50:47 +0900

    change coordinates

--- coordinates/main.js
+++ coordinates/main.js
@@ -4,21 +4,25 @@ const vertical = document.querySelector('.vertical');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-document.addEventListener('mousemove', (e)=>{
-    console.log(e.clientX)
-    console.log(e.clientY)
-    let x = e.clientX
-    let y = e.clientX
-    horizontal.style.top = `${y}px`
-    vertical.style.left = `${x}px`
-    target.style.top = `${y}px`
-    target.style.left = `${x}px`
-    tag.style.top = `${y}px`
-    tag.style.left = `${x}px`
-    tag.innerHTML = `${x}px, ${y}px`
+addEventListener('load', ()=>{
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width /2;
+    const targetHalfHeith = targetRect.height /2;
+
+    document.addEventListener('mousemove', (e)=>{
+        let x = e.clientX
+        let y = e.clientX
+    
+        vertical.style.transform = `translateX(${x}px)`
+        horizontal.style.transform = `translateY(${y}px)`
+        target.style.transform = `translate(${x}px, ${y}px)`
+        tag.style.transform = `translate(${x}px, ${y}px)`
+        tag.innerHTML = `${x}px, ${y}px`
+        
+    
+    })
+
 })
 
-function getTag(e){
-    console.log(e.clientX)
-    console.log(e.clientY)
-}
+
+
