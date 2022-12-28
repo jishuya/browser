cae0fa80a57c442ce2e3066481e513df0d52ff29
jishuya <jishuya3015@gmail.com>
2022-12-28 23:53:47 +0900

    performance improvement

--- coordinates/main.js
+++ coordinates/main.js
@@ -1,19 +1,18 @@
-
-const horizontal = document.querySelector('.horizontal');
-const vertical = document.querySelector('.vertical');
-const target = document.querySelector('.target');
-const tag = document.querySelector('.tag');
-
 document.addEventListener('mousemove', (e)=>{
-    let x = e.clientX
-    let y = e.clientY
+    const x = e.clientX
+    const y = e.clientY
 
-    vertical.style.left = `${x}px`
+    const horizontal = document.querySelector('.horizontal')
+    const vertical = document.querySelector('.vertical')
+    const target = document.querySelector('.target')
+    const tag = document.querySelector('.tag')
+
     horizontal.style.top = `${y}px`
-    target.style.left = `${x}px`
+    vertical.style.left = `${x}px`
     target.style.top = `${y}px`
-    tag.style.left = `${x}px`
+    target.style.left = `${x}px`
     tag.style.top = `${y}px`
-    tag.innerHTML = `${x}px, ${y}px`  
+    tag.style.left = `${x}px`
+    tag.innerHTML = `${x}px, ${y}px`
+    
 })
-
--- coordinates2/main.js
+++ coordinates2/main.js
@@ -1,28 +1,22 @@
-
-const horizontal = document.querySelector('.horizontal');
-const vertical = document.querySelector('.vertical');
-const target = document.querySelector('.target');
-const tag = document.querySelector('.tag');
+const horizontal = document.querySelector('.horizontal')
+    const vertical = document.querySelector('.vertical')
+    const target = document.querySelector('.target')
+    const tag = document.querySelector('.tag')
 
 addEventListener('load', ()=>{
-    const targetRect = target.getBoundingClientRect();
-    const targetHalfWidth = targetRect.width /2;
-    const targetHalfHeigth = targetRect.height /2;
-
     document.addEventListener('mousemove', (e)=>{
-        let x = e.clientX
-        let y = e.clientY
+        const x = e.clientX
+        const y = e.clientY
+        const targetRect = target.getBoundingClientRect();
+        const halfWeight = targetRect.width / 2;
+        const halfHeight = targetRect.height / 2;
     
-        vertical.style.transform = `translateX(${x}px)`
         horizontal.style.transform = `translateY(${y}px)`
-        target.style.transform = `translate(${x-targetHalfWidth}px, ${y- targetHalfHeigth}px)`
+        vertical.style.transform = `translateX(${x}px)`
+        target.style.transform = `translate(${x-halfWeight}px, ${y - halfHeight}px)`
         tag.style.transform = `translate(${x}px, ${y}px)`
         tag.innerHTML = `${x}px, ${y}px`
-
-    
+        
     })
-
+    
 })
-
-
-
