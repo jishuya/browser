37cf527948b585ace7dad56adef030d12c3cdb64
= <jishuya3015@gmail.com>
2023-03-03 09:38:24 +0900

    delegation added

--- shopping2/main.js
+++ shopping2/main.js
@@ -22,35 +22,6 @@ function onAdd(){
 
 let id = 0;
 function createItem(text){
-    // const itemRow = document.createElement('li');
-    // itemRow.setAttribute('class', 'item__row');
-
-    // const item = document.createElement('div');
-    // item.setAttribute('class', 'item');
-
-    // const name = document.createElement('span');
-    // name.setAttribute('class', 'item__name');
-    // name.innerText = text;
-
-    // const deleteBtn = document.createElement('button');
-    // deleteBtn.setAttribute('class', 'item__delete');
-    // deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-
-    // deleteBtn.addEventListener('click', ()=>{
-    //     items.removeChild(itemRow)
-    // })
-
-    // const itemDivider = document.createElement('div');
-    // itemDivider.setAttribute('class', 'item__divider');
-
-    // item.appendChild(name);
-    // item.appendChild(deleteBtn);
-    // console.log(item)
-
-    // itemRow.appendChild(item);
-    // itemRow.appendChild(itemDivider);
-    // return itemRow
-
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item__row');
     itemRow.setAttribute('data-id', id);
